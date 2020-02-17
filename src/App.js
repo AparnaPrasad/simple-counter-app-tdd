7ed6acaa0f4ab0b45f6f837a567c4c960667288e
Aparna Bangalore <aparna.bangalore@misterclipping.com>
2020-02-17 11:57:00 +0100

    simple tdd counter app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            counter: 0,
+            showErrorMsg: false
+        }
+    }
+
+    decrementCounter = () => {
+        const { counter } = this.state;
+        if (counter) {
+            this.setState({
+                counter: counter - 1,
+                showErrorMsg: false
+            });
+            return;
+        }
+        this.setState({ showErrorMsg: true });
+    }
+
+    render() {
+        const { counter, showErrorMsg } = this.state;
+        return (
+            <div className="App" data-test-id='app-component-id'>
+                <button data-test-id='increment-button-element-id' onClick={() => { this.setState({ counter: counter + 1 }) }}>Increment</button>
+                <button data-test-id='decrement-button-element-id' onClick={() => { this.decrementCounter() }}>Decrement</button>
+                <h3 data-test-id='counter-display-element-id'>The counter is: {counter}</h3>
+                {showErrorMsg && <h5 data-test-id='counter-error-display-element-id'>Counter cannot be less than 0</h5>}
+
+            </div>
+        )
+    }
+  
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,110 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const renderComponent = (props = {}, state = null) => {
+    const wrapper = shallow(<App {...props} />);
+    if (state) {
+        wrapper.setState(state)
+    }
+    return wrapper;
+}
+
+const getElement = (wrapper, id) => {
+    return wrapper.find(`[data-test-id="${id}"]`)
+}
+
+describe('renders the component with all its elements', () => {
+    test('renders the component', () => {
+        const wrapper = renderComponent()
+        const appCompoent = getElement(wrapper, 'app-component-id')
+        expect(appCompoent.length).toBe(1)
+    });
+
+    test('renders counter display message', () => {
+        const wrapper = renderComponent()
+        const counterDisplay = getElement(wrapper, 'counter-display-element-id')
+        expect(counterDisplay.length).toBe(1)
+    });
+
+    test('renders increment button', () => {
+        const wrapper = renderComponent()
+        const incrementButton = getElement(wrapper, 'increment-button-element-id')
+        expect(incrementButton.length).toBe(1)
+    });
+
+    test('renders decrement button', () => {
+        const wrapper = renderComponent()
+        const decrementButton = getElement(wrapper, 'decrement-button-element-id')
+        expect(decrementButton.length).toBe(1)
+    });
+
+    //test('renders error message only on click and not when counter is 0', () => {
+    //    const counter = 0;
+    //    const wrapper = renderComponent(null, { counter })
+    //    const counterErrorDisplay = getElement(wrapper, 'counter-error-display-element-id')
+    //    expect(counterErrorDisplay.length).toBe(0)
+    //})
 });
+
+describe('test component state', () => {
+    test('should render default counter state 0', () => {
+        const wrapper = renderComponent();
+        const counterState = wrapper.state('counter');
+        expect(counterState).toBe(0);
+    });
+});
+
+describe('test increment action', () => {
+    test('clicking increment button increment the count', () => {
+        const counter = 7
+        const wrapper = renderComponent(null, { counter })
+        const incrementButton = getElement(wrapper, 'increment-button-element-id')
+        incrementButton.simulate('click');
+        const counterDisplay = getElement(wrapper, 'counter-display-element-id')
+        expect(counterDisplay.text()).toContain(counter + 1)
+    });
+
+    test('clicking decrement button decrements the counter', () => {
+        const counter = 7
+        const wrapper = renderComponent(null, { counter })
+        const decrementButton = getElement(wrapper, 'decrement-button-element-id')
+        decrementButton.simulate('click');
+        const counterDisplay = getElement(wrapper, 'counter-display-element-id')
+        expect(counterDisplay.text()).toContain(counter - 1)
+
+    });
+
+    test('click decrement button at counter 0 does not decrement the counter', () => {
+        const counter = 0
+        const wrapper = renderComponent(null, { counter })
+        const decrementButton = getElement(wrapper, 'decrement-button-element-id')
+        decrementButton.simulate('click');
+        const counterDisplay = getElement(wrapper, 'counter-display-element-id')
+        expect(counterDisplay.text()).toContain(counter)
+    })
+
+    test('click decrement button at counter 0 shows error message', () => {
+        const counter = 0
+        const wrapper = renderComponent(null, { counter })
+        const decrementButton = getElement(wrapper, 'decrement-button-element-id')
+        let counterErrorDisplay = getElement(wrapper, 'counter-error-display-element-id')
+        expect(counterErrorDisplay.length).toBe(0) //before click
+
+        decrementButton.simulate('click')
+        counterErrorDisplay = getElement(wrapper, 'counter-error-display-element-id')
+        expect(counterErrorDisplay.length).toBe(1)
+    });
+
+    test('click increment button at counter 0 does not show error message', () => {
+        const counter = 0
+        const wrapper = renderComponent(null, { counter })
+        const incrementButton = getElement(wrapper, 'increment-button-element-id')
+        incrementButton.simulate('click')
+        const counterErrorDisplay = getElement(wrapper, 'counter-error-display-element-id')
+        expect(counterErrorDisplay.length).toBe(0)
+    });
+
+});
+
+
